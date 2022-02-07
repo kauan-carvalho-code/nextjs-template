ecd7c6a76d9f4edfbe2fa6cb909680469a244d5c
Kauan Carvalho <kauancarvalho987@gmail.com>
2022-02-07 15:10:50 -0300

    impl: styled-components

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>NEXTJS-TEMPLATE</title>
+        <meta name="description" content="created by: Kauan Carvalho" />
+      </Head>
+      <body>
+        <h1>Next.js template using: Typescript, Styled-components, eslint and prettier</h1>
+      </body>
+    </div>
+  )
+}
+
+export default Home
